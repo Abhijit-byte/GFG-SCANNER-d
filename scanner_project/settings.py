876c861ebd877e0889e6b3ff384c64723faef209
abhijitdash5370 <abhijitdash5370@gmail.com>
2025-12-18 02:41:08 +0530

    changedes

--- scanner_project/settings.py
+++ scanner_project/settings.py
@@ -1,6 +1,15 @@
 """
 Django settings for scanner_project project.
 """
+import os
+import dj_database_url
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 from pathlib import Path
 import os
@@ -63,11 +72,13 @@ WSGI_APPLICATION = 'scanner_project.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
